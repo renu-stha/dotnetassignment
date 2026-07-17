b1647bbe8211df4f9fdc7ca1c56e9136469f4c92
renu-stha <164057920+renu-stha@users.noreply.github.com>
2026-07-17 22:59:07 +0545

    Add files via upload

---
+++ assignment3_jagged.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[][] m = { new[] { 80, 90, 85 }, new[] { 75, 88 }, new[] { 92, 95, 89, 100 } };
+        for (int i = 0; i < m.Length; i++)
+        {
+            Console.Write($"Student {i + 1}: ");
+            foreach (var x in m[i]) Console.Write(x + " "); Console.WriteLine();
+        }
+    }
+}
