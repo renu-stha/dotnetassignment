4f08908f7fd40d6e1b611190fb88dfce94e3f776
renu-stha <164057920+renu-stha@users.noreply.github.com>
2026-07-17 23:04:49 +0545

    Add files via upload

---
+++ Assignment_delegant.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class Publisher
+{
+    public delegate void Notify(); public event Notify Done;
+    public void Start() { Console.WriteLine("Processing..."); Done?.Invoke(); }
+}
+class Subscriber { public void Msg() => Console.WriteLine("Completed notification received."); }
+class Program { static void Main() { var p = new Publisher(); var s = new Subscriber(); p.Done += s.Msg; p.Start(); } }
