de4263b5965e81e7485c8722d5b3a68e1b60d3cd
renu-stha <164057920+renu-stha@users.noreply.github.com>
2026-07-17 22:32:20 +0545

    Add files via upload

---
+++ 3D multidimensional array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,,] arr = new int[2, 2, 2];
+        int value = 1;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    arr[i, j, k] = value++;
+                }
+            }
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine("Layer " + i);
+
+            for (int j = 0; j < 2; j++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    Console.Write(arr[i, j, k] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ indexer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Student
+{
+    string[] n = new string[5];
+    public string this[int i] { get => n[i]; set => n[i] = value; }
+}
+class Program
+{
+    static void Main()
+    {
+        var s = new Student();
+        s[0] = "Ram"; s[1] = "Kalpana"; s[2] = "Hema"; s[3] = "Prem"; s[4] = "Gita";
+        for (int i = 0; i < 5; i++) Console.WriteLine(s[i]);
+    }
+}
